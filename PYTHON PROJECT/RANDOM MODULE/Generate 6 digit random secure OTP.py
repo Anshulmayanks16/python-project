66ade6a64e73079e136a1838534dd22147f64603
Anshul  Rajput <94006502+Anshulmayanks16@users.noreply.github.com>
2022-05-31 12:47:41 +0530

    Create Generate 6 digit random secure OTP.py

---
+++ PYTHON PROJECT/RANDOM MODULE/Generate 6 digit random secure OTP.py
@@ -0,0 +1,9 @@
+import secrets
+
+#Getting systemRandom class instance out of secrets module
+secretsGenerator = secrets.SystemRandom()
+
+print("Generating 6 digit random OTP")
+otp = secretsGenerator.randrange(100000, 999999)
+
+print("Secure random OTP is ", otp)
